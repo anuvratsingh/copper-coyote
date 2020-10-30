5cdab0ab9700808df46eb4b38a2dd0e003c64aa7
Anuvrat <anuvrats06@gmail.com>
2020-10-31 00:21:37 +0530

    working logs

---
+++ target/rls/debug/deps/copper_coyote-78860dee64e57a7c.d
@@ -0,0 +1,5 @@
+c:\Users\Anuvrat\Desktop\Projects\rust\copper_coyote\target\rls\debug\deps\copper_coyote-78860dee64e57a7c.rmeta: src\main.rs
+
+c:\Users\Anuvrat\Desktop\Projects\rust\copper_coyote\target\rls\debug\deps\copper_coyote-78860dee64e57a7c.d: src\main.rs
+
+src\main.rs:
---
+++ target/rls/debug/deps/copper_coyote-9b7876af3e31db57.d
@@ -0,0 +1,5 @@
+c:\Users\Anuvrat\Desktop\Projects\rust\copper_coyote\target\rls\debug\deps\copper_coyote-9b7876af3e31db57.rmeta: src\main.rs
+
+c:\Users\Anuvrat\Desktop\Projects\rust\copper_coyote\target\rls\debug\deps\copper_coyote-9b7876af3e31db57.d: src\main.rs
+
+src\main.rs:
